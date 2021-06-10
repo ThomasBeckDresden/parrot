da1766f9ea70b179fe0b8924b73f9588343425b6
ThomasBeckDresden <thomas.beck.dresden@gmail.com>
2021-06-10 21:44:26 +0200

    started

--- src/UserAccountDetails.js
+++ src/UserAccountDetails.js
@@ -1,14 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 export default function UserAccountDetails() {
-    return (
-<div className="col-4">   
-        <div className="row FullName">FullName</div>
-        <div className="row Nickname">Nickname</div>
-        <div className="row UserPicture">UserPicture</div>
-        <div className="row UserDescription">UserDescription</div>
-        <div className="row UserLocation">UserLocation</div>
-        <div className="row NumberOfWhistles">NumberOfWhistles</div> 
-</div>
-    );
+  const [loggedInUser, setUser] = useState();
+  const fetchLogin = async () => {
+    try {
+      const { data: userData } = await axios.get(
+        `https://mini-twitter-back.herokuapp.com/me`
+      );
+      setUser(userData);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+  fetchLogin();
+  console.log(loggedInUser);
+  return (
+    <div className="col-4">
+      <div className="row FullName">FullName</div>
+      <div className="row Nickname">Nickname</div>
+      <div className="row UserPicture">UserPicture</div>
+      <div className="row UserDescription">UserDescription</div>
+      <div className="row UserLocation">UserLocation</div>
+      <div className="row NumberOfWhistles">NumberOfWhistles</div>
+    </div>
+  );
 }
