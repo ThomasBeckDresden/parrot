ed1321a578b8a06233775ef80eec4db3c3b61662
zinaelnahel <zinamahmoud1989@gmail.com>
2021-06-11 10:02:52 +0200

    add styling

--- src/Header.js
+++ src/Header.js
@@ -2,24 +2,25 @@ import parrot from "./parrot.jpg";
 
 export default function Header() {
   return (
-    <div class="container-fluid header-part d-flex">
-      <div className="me-auto">
-        <form class="d-flex ">
+    <>
+      <div className=" container container-fluid position-absolute  top-0 end-0 ">
+        <img src={parrot} alt="parrot-logo" className="parrotLogo float-end" />
+
+        <h1 className="float-end " style={{ color: "red" }}>
+          Parrot
+        </h1>
+        <form className="d-flex align-center w-25  searchForm ">
           <input
-            class="form-control me-2"
+            className="form-control me-2 "
             type="search"
             placeholder="Search keyword"
             aria-label="Search"
           />
-          <button class="btn btn-outline-success" type="submit">
+          <button class="btn btn-danger" type="submit">
             Search
           </button>
         </form>
       </div>
-      <div className="  justify-content-end ">
-        <h1 className="align-self-center">Parrot</h1>
-        <img src={parrot} alt="parrot-logo" className="parrotLogo" />
-      </div>
-    </div>
+    </>
   );
 }
--- src/UserAccountDetails.js
+++ src/UserAccountDetails.js
@@ -1,14 +1,26 @@
 import React from "react";
 
 export default function UserAccountDetails() {
-    return (
-<div className="col-4">   
-        <div className="row FullName">FullName</div>
-        <div className="row Nickname">Nickname</div>
-        <div className="row UserPicture">UserPicture</div>
-        <div className="row UserDescription">UserDescription</div>
-        <div className="row UserLocation">UserLocation</div>
-        <div className="row NumberOfWhistles">NumberOfWhistles</div> 
-</div>
-    );
+  return (
+    <div className="col-2 profiletext ">
+      <div class="card" style={{ width: "18rem", height: "30rem" }}>
+        <div className="card-body">
+          <p className="card-text ">
+            <div className="row FullName">FullName</div>
+            <br />
+            <div className="row Nickname">Nickname</div>
+            <br />
+            <div className="row UserPicture">UserPicture</div>
+            <br />
+            <div className="row UserDescription">UserDescription</div>
+            <br />
+            <div className="row UserLocation">UserLocation</div>
+            <br />
+            <div className="row NumberOfWhistles">NumberOfWhistles</div>
+            <br />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
